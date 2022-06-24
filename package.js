1f2d9fa3a3f7ec3a00f0f8a2f8b6acd12edb8e13
zodern <zodern@users.noreply.github.com>
2022-06-24 12:06:44 -0500

    Update summary

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'montiapm:flow-router',
-  summary: 'Drop in replacement for flow router with support for Meteor 2.3+',
+  summary: 'Drop in replacement for flow router with support for Meteor 1.3 - 2.3+',
   version: '2.12.1',
   git: 'https://github.com/kadirahq/flow-router.git'
 });
