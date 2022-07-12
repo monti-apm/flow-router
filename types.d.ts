e4ed58d891a6b363d3f98205a57dd84c06a856fc
zodern <zodern@users.noreply.github.com>
2022-07-12 15:16:40 -0500

    Improve types

--- types.d.ts
+++ types.d.ts
@@ -1,63 +1,61 @@
-declare module 'meteor/montiapm:flow-router' {
-  type Trigger = (
-    context: {},
-    doRedirect: (
-      url: string,
-      params?: { key: string },
-      queryParams?: { [key: string]: string }
-    ) => void,
-    doStop: () => void
-  ) => void;
 
-  type TriggerFilter = {
-    only?: string[];
-    except?: string[];
-  }
-  type TriggerFilterParam = TriggerFilter | TriggerFilter[];
+type Trigger = (
+  context: {},
+  doRedirect: (
+    url: string,
+    params?: { key: string },
+    queryParams?: { [key: string]: string }
+  ) => void,
+  doStop: () => void
+) => void;
 
-  interface FlowRouter {
-    getQueryParam: (param: string) => string | undefined;
-    getParam: (param: string) => string | undefined;
-    subscriptions?: any;
-    register: (name: string, sub: any) => void;
-    route: (path: string, options?: {
-      name?: string;
-      action?: () => void;
-      subscriptions?: (params: {[key: string]: string}) => void;
-      triggersEnter?: Trigger[];
-      triggersExit?: Trigger[];
-    }, group?: any) => any;
-    group: (options: any) => any;
-    path: (
-      pathDef: string,
-      fields?: { [key: string]: string },
-      queryParams?: { [key: string]: string }
-    ) => string;
-    go: (
-      pathDef: string,
-      fields?: { [key: string]: string },
-      queryParams?: { [key: string]: string }
-    ) => void;
-    reload: () => void;
-    redirect: (path: string) => void;
-    setQueryParams: (
-      newParams: { [key: string]: string | null | undefined }
-    ) => boolean;
-    current: () => {
-      path: string;
-      context: any;
-      params: string[];
-      queryParams: { [key: string]: string };
-    };
-    getRouteName: () => string;
-    watchPathChange: () => void;
-    subsRead: () => boolean;
-    triggers: {
-      enter: (triggers: Trigger[], filter?: TriggerFilterParam) => void;
-      exit: (triggers: Trigger[], filter?: TriggerFilterParam) => void;
-    }
-  }
-
-  var FlowRouter: FlowRouter;
-  export { FlowRouter };
+type TriggerFilter = {
+  only?: string[];
+  except?: string[];
 }
+type TriggerFilterParam = TriggerFilter | TriggerFilter[];
+
+interface Router {
+  getQueryParam: (param: string) => string | undefined;
+  getParam: (param: string) => string | undefined;
+  subscriptions?: any;
+  register: (name: string, sub: any) => void;
+  route: (path: string, options?: {
+    name?: string;
+    action?: () => void;
+    subscriptions?: (params: { [key: string]: string }) => void;
+    triggersEnter?: Trigger[];
+    triggersExit?: Trigger[];
+  }, group?: any) => any;
+  group: (options: any) => any;
+  path: (
+    pathDef: string,
+    fields?: { [key: string]: string },
+    queryParams?: { [key: string]: string }
+  ) => string;
+  go: (
+    pathDef: string,
+    fields?: { [key: string]: string },
+    queryParams?: { [key: string]: string }
+  ) => void;
+  reload: () => void;
+  redirect: (path: string) => void;
+  setQueryParams: (
+    newParams: { [key: string]: string | null | undefined }
+  ) => boolean;
+  current: () => {
+    path: string;
+    context: any;
+    params: string[];
+    queryParams: { [key: string]: string };
+  };
+  getRouteName: () => string;
+  watchPathChange: () => void;
+  subsRead: () => boolean;
+  triggers: {
+    enter: (triggers: Trigger[], filter?: TriggerFilterParam) => void;
+    exit: (triggers: Trigger[], filter?: TriggerFilterParam) => void;
+  }
+}
+
+export const FlowRouter: Router;
